096f6390e77a6eb1a22d2326601b9d1bbdbfdfd6
Monica <monica@newgendigital.com>
2024-09-03 12:03:24 +0530

    grants.tf issues fixed

--- modules/snowflake_resources/grants.tf
+++ modules/snowflake_resources/grants.tf
@@ -1,7 +1,6 @@
 resource "snowflake_grant_privileges_to_account_role" "database_ro_grant" {
   privileges = ["USAGE"]
   account_role_name = "DEV_PCDR_QA"
-  on_account = true
   on_account_object {
     object_type = "DATABASE"
     object_name = snowflake_database.tf_demo_database.name
@@ -11,10 +10,8 @@ resource "snowflake_grant_privileges_to_account_role" "database_ro_grant" {
 resource "snowflake_grant_privileges_to_account_role" "schema_ro_grant" {
   privileges = ["USAGE"]
   account_role_name = "DEV_PCDR_QA"
-    on_account        = true
-  on_account_object {
-    object_type = "DATABASE"
-    object_name = snowflake_database.tf_demo_database.name
+  on_schema {
+    schema_name = "\"${snowflake_database.tf_demo_database.name}\".\"${snowflake_schema.tf_demo_schema.name}\"" # note this is a fully qualified name!
   }
 }
 
@@ -22,24 +19,29 @@ resource "snowflake_grant_privileges_to_account_role" "table_ro_grant" {
   privileges = ["SELECT"]
   account_role_name     = "DEV_PCDR_QA"
   with_grant_option = false
-  on_schema {
-    schema_name = snowflake_schema.tf_demo_schema.name
-  }  
+  on_schema_object {
+    all {
+      object_type_plural = "TABLES"
+      in_schema          = "\"${snowflake_database.tf_demo_database.name}\".\"${snowflake_schema.tf_demo_schema.name}\"" # note this is a fully qualified name!
+    }
+  } 
 }
 
 resource "snowflake_grant_privileges_to_account_role" "view_ro_grant" {
-  privileges = ["SELECT"]
+  privileges = ["SELECT", "REFERENCES"]
   account_role_name     = "DEV_PCDR_QA"
   with_grant_option = false
-  on_schema {
-    schema_name = snowflake_schema.tf_demo_schema.name
-  }
+  on_schema_object {
+    all {
+      object_type_plural = "VIEWS"
+      in_schema          = "\"${snowflake_database.tf_demo_database.name}\".\"${snowflake_schema.tf_demo_schema.name}\"" # note this is a fully qualified name!
+    }
+  } 
 }
 
 resource "snowflake_grant_privileges_to_account_role" "warehouse_grant" {
   privileges = ["USAGE"]
   account_role_name     = "DEV_PCDR_QA"
-  on_account        = true
   on_account_object {
     object_type = "WAREHOUSE"
     object_name = snowflake_warehouse.task_warehouse.name
