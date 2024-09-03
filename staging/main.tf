aeb4ae15cad967fd803fe5f4c6c0e4fceb181382
Monica <monica@newgendigital.com>
2024-09-03 10:20:17 +0530

    main.tf error fixed -2

--- staging/main.tf
+++ staging/main.tf
@@ -8,11 +8,10 @@ terraform {
 }
 
 provider "snowflake" {
-  username    = "tf-snow"
-  account     = "KBXIPRA-PBS"
-  role        = "dev_sys_admin"
-  private_key_path = "~/.ssh/snowflake_tf_snow_key.pem"
-  private_key = var.snowflake_private_key
+  user             = "tf-snow"
+  account          = "KBXIPRA-PBS"
+  role             = "dev_sys_admin"
+  private_key      = var.snowflake_private_key
 }
 
 module "snowflake_resources" {
